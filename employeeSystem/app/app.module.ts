d37fb92fd5ce8d0d1321a3f0d3e24a12374b06c3
muthupandy1332 <124023406+muthupandy1332@users.noreply.github.com>
2023-07-18 10:06:44 +0530

    angular Crud

---
+++ employeeSystem/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { CreateDepartmentComponent } from './create-department/create-department.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateEmployeeComponent,
+    EmployeelistComponent,
+    UpdateEmployeeComponent,
+    DepartmentListComponent,
+    CreateDepartmentComponent,
+    EmployeeDetailsComponent
+   
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
