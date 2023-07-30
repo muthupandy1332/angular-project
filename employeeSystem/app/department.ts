db429054a4bf7e797dc6d0ed01c343dde212f3d1
muthupandy1332 <124023406+muthupandy1332@users.noreply.github.com>
2023-07-30 17:44:19 +0530

    Delete employeeSystem/app directory

--- employeeSystem/app/department.ts
+++
@@ -1,9 +0,0 @@
-import { Employee } from "./employee";
-
-export class Department {
-    deptId!:number;
-    deptName!:string;
-    manager!:string;
-    description!:string;
-    employee!: Employee;
-}
